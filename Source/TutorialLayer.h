993df283836563e7d139933c009436940497e902
Mark Barrasso <mark.barrasso@live.com>
2015-02-07 18:47:04 -0500

    added tutorial layer

---
+++ Source/TutorialLayer.h
@@ -0,0 +1,13 @@
+//
+//  TutorialLayer.h
+//  Deditio
+//
+//  Created by Mark on 2/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface TutorialLayer : CCNode
+
+@end
---
+++ Source/TutorialLayer.m
@@ -0,0 +1,13 @@
+//
+//  TutorialLayer.m
+//  Deditio
+//
+//  Created by Mark on 2/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "TutorialLayer.h"
+
+@implementation TutorialLayer
+
+@end
